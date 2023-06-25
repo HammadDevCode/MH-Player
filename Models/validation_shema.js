d675a11c4aab6b30d276f78bdcc0c042d22e4fc9
HammadDevCode <hammaddevcode@gmail.com>
2023-06-25 15:00:27 +0500

    confirmPassword

--- Models/validation_shema.js
+++ Models/validation_shema.js
@@ -5,8 +5,13 @@ const joi = require("@hapi/joi")
 const AuthRegistrationSchema = joi.object({
     email:joi.string().email().lowercase().required(),
     password: joi.string().min(8).required(),
+    confirmPassword: joi.ref('password'),
     firstName: joi.string().min(3).required(),
-    lastName: joi.string().min(3).required()
+    lastName: joi.string().min(3).required(),
+    cloningDetection: {
+        appInstalledPath: joi.string().required().label("Problem with Device"),
+        packageName: joi.string().required().label("Problem with Device")
+    }
 })
 
 
